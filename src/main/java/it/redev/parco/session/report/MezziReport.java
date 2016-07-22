2c688e859584002abb8338dae919b837e34d6b13
Gotamo <massimoromano3@gmail.com>
2016-07-22 12:39:54 +0200

    Aggiornata licenza

--- src/main/java/it/redev/parco/session/report/MezziReport.java
+++ src/main/java/it/redev/parco/session/report/MezziReport.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
-* Inspired Model Exporter is a framework to export data from pojo class.
+* Parco è un applicativo per la gestione dei consumi del parco auto
 * Copyright (C) 2016 Inspired Soft
 * 
 * This program is free software: you can redistribute it and/or modify
